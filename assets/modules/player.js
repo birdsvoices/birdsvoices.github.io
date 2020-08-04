492634568ae03439476d6d2ca81d7e08c7dd16c7
Селезень <53906410+loxleu@users.noreply.github.com>
2020-08-04 17:58:01 +0300

    Player module improvements

--- assets/modules/player.js
+++ assets/modules/player.js
@@ -3,7 +3,13 @@
  * @description This class represents a video or audio player interface.
  */
 export class PlayerInterface {
+    /**
+     * @description Main constructor of the class
+     * 
+     * @constructor
+     * @param {String} type - Represents the type of the player. Possible values are "audio", "video" or "gif" 
+     */
     constructor(type) {
-        
+        this.type = type;
     }
 }
