52f72f203d9fa83cdb23184434402f30d6822e8c
Селезень <53906410+loxleu@users.noreply.github.com>
2020-08-24 16:30:47 +0300

    Improved console module

--- assets/modules/console.ts
+++ assets/modules/console.ts
@@ -4,4 +4,23 @@ class Console {
     public static Print(...args: any): void {
         console.log(args.join(' '));
     }
+    public static PrintError(...args: any): void {
+        console.error(args.join(' '));
+    }
+    public static PrintWarn(...args: any): void {
+        console.warn(args.join(' '));
+    }
+    public static PrintInfo(...args: any): void {
+        console.info(args.join(' '));
+    }
+    public static Clear() {
+        console.info('Clearing the console...');
+        try {
+            console.clear();
+        } catch (err) {
+            if (err.name == ('SyntaxError' || 'TypeError' || 'ReferenceError' || 'Error')) {
+                console.error(`An error occured while trying to clear the console!\n${err.name}: ${err.message}\n at ${err.stack}`)
+            } else throw err;
+        }
+    }
 }
