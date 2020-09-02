075bd4f80ceaab9eb080f6a8116e5a46e9ef4c2a
Селезень <53906410+loxleu@users.noreply.github.com>
2020-09-02 22:02:10 +0300

    Added not-working-at-the-momnet animation

--- source/scripts/main.jsx
+++ source/scripts/main.jsx
@@ -3,3 +3,8 @@
 $('window').ready(() => {
     $('.header').height($(window).height());
 });
+$('.navbar a').click(function() {
+    $('body, html').animate({
+        scrollTop: $('#' + $(this).data('value')).offset().top,
+    }, 1000);
+});
